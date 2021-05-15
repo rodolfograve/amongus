8bd4987b513ff1608d1f780d06866cb763600b6a
Rodolfo Grave <rodolfograve@gmail.com>
2021-05-15 20:35:51 +0100

    Fixed deployment script

--- deploy/Publish.ps1
+++ deploy/Publish.ps1
@@ -6,4 +6,4 @@ If (Test-Path $PublishPath) {
 dotnet publish ..\AmongUsRoleSelector\AmongUsRoleSelector.csproj --configuration Release --output ..\publish
 azcopy sync "..\publish\wwwroot" "https://amongus.blob.core.windows.net/`$web" --put-md5 --delete-destination=true --recursive=true
 
-Unpublish-AzCdnEndpointContent -EndpointName  "amongus"  -ProfileName  "amongus"  -ResourceGroupName  "amongus"  -PurgeContent  @( "/*" )  -Force
+Unpublish-AzCdnEndpointContent -EndpointName  "amongus"  -ProfileName  "amongus"  -ResourceGroupName  "amongus"  -PurgeContent  @( "/*" ) -Confirm:$false
